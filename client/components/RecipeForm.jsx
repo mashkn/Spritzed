de9525e8efe2158f86937b24aaa96b7b1c881237
mashkn <mariya.novokhatska@gmail.com>
2022-12-21 07:09:07 -0700

    updates

--- client/components/RecipeForm.jsx
+++ client/components/RecipeForm.jsx
@@ -7,17 +7,19 @@ const RecipeForm = props => {
 
     //button click handler
     const createRecipe = () => {
-        console.log(recipeName, 'HERE IS THE NAME OF THE RECIPE!')
+        const recipe = { name: 'test' };
+        axios.post('/recipe/create', recipe)
     }
+    console.log('recipe form hello')
 
     return (
         <div className="RecipeForm">
             Create Recipe
-            <form method='POST' action='/create'>
-                <input name="name" type="text" placeholder="Recipe Name" onChange={(event) => setRecipeName(event.target.value)}></input>
-                <input name="image" type="image"></input>
-                <input name="ingredient1" type="text" placeholder="Ingredient"><input name="measure1" type="text" placeholder="Volume"></input></input>
-                <button type="submit" value="create recipe" onClick={createRecipe}></button>
+            <form>
+                <input name="name" type="text" placeholder="Recipe Name"></input>
+                <input name="image" type="file"></input>
+                <input name="ingredient1" type="text" placeholder="Ingredient"></input><input name="measure1" type="text" placeholder="Volume"></input>
+                <button type="submit" value="create recipe" onClick={createRecipe}>Create Recipe</button>
             </form>
         </div>
     )
